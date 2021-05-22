611f088c7da8b779b08bfac6614aab8fbcdd4672
nara58 <80908498+nara58@users.noreply.github.com>
2021-05-22 16:29:20 +0700

    Update clientimage.py

--- clientimage.py
+++ clientimage.py
@@ -1,6 +1,6 @@
 import requests
 
-resp=requests.post("http://localhost:5000/predict",
+resp=requests.post("http://localhost:5000",
                     files={"file": open('D://olahcitra//dog.jpg','rb')})
                     
          
